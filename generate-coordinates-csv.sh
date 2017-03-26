c318f3ccb61ba08649b74beb262abde6671b333f
Aleksandrs Antonovs <alex.ant0303@gmail.com>
2017-03-26 16:32:37 +0300

    added random coordinates generator and a big CSV file

---
+++ generate-coordinates-csv.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+AMOUNT=1000000
+OUTPUT=geoBig.csv
+
+# randomly select either negative or positive value
+function neg {
+  rand=`shuf -i 0-1 -n 1`
+  if [ $rand -eq 0 ]; then
+    echo -
+  else
+    echo
+  fi
+}
+
+echo '"id","lat","lng"' > $OUTPUT
+
+for i in `seq 1 $AMOUNT`
+do
+  id=$i
+  lat=`neg``shuf -i 0-90 -n 1`.`shuf -i 0-999999 -n 1`
+  lng=`neg``shuf -i 0-180 -n 1`.`shuf -i 0-999999 -n 1`
+  echo $id,$lat,$lng >> $OUTPUT
+done
